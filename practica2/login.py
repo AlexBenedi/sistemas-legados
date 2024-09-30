d7632fadb03b2612932055ec330f30b2a6f9199f
AlexBenedi <843826@unizar.es>
2024-09-30 14:57:55 +0200

    Migración ttk

--- practica2/login.py
+++ practica2/login.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import tkinter as tk
+import tkinter as ttk
 
 def login():
     
@@ -7,23 +7,23 @@ def login():
     password = entry_password.get()
 
 # Crear ventana principal
-root = tk.Tk()
+root = Tk()
 root.title("Login")
 root.geometry("300x200")
 
 # Etiquetas y entradas de usuario y contraseña
-label_username = tk.Label(root, text="Usuario:")
+label_username = ttk.Label(root, text="Usuario:")
 label_username.pack(pady=10)
-entry_username = tk.Entry(root)
+entry_username = ttk.Entry(root)
 entry_username.pack()
 
-label_password = tk.Label(root, text="Contraseña:")
+label_password = ttk.Label(root, text="Contraseña:")
 label_password.pack(pady=10)
-entry_password = tk.Entry(root, show="*")
+entry_password = ttk.Entry(root, show="*")
 entry_password.pack()
 
 # Botón de inicio de sesión
-button_login = tk.Button(root, text="Iniciar sesión", command=login)
+button_login = ttk.Button(root, text="Iniciar sesión", command=login)
 button_login.pack(pady=20)
 
 # Iniciar la aplicación
