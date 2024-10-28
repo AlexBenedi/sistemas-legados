86fe148647b2af3f2f081f6d1e93cc3031017169
Andreiiii19 <dumbravaandrei5@gmail.com>
2024-10-28 15:12:00 +0100

    busqueda por nombre

--- practica3/gameslist/gameslist/views.py
+++ practica3/gameslist/gameslist/views.py
@@ -27,26 +27,25 @@ def index(request):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     bat_file_path = os.path.join(base_dir, 'Database-MSDOS')
     os.chdir(bat_file_path)
+    subprocess.Popen("database.bat")
+    time.sleep(5)
+    
+    juegos = []
+    num_archivos = 0
+    
+    pyautogui.write('4')
+    pyautogui.hotkey('ctrl', 'f9')
+    time.sleep(0.5)
+    
+    with open('Database/DATABASE.TXT', 'r') as file:
+        for line in file:
+            if 'CONTIENE' in line:
+                num_archivos = int(line.split()[1])
+                break
     
     query = request.GET.get('q')
     print("Query: ", query)
-    if query is None:
-        subprocess.Popen("database.bat")
-        time.sleep(5)
-        
-        juegos = []
-        global num_archivos 
-        num_archivos = 0
-        
-        pyautogui.write('4')
-        pyautogui.hotkey('ctrl', 'f9')
-        time.sleep(0.5)
-        
-        with open('Database/DATABASE.TXT', 'r') as file:
-            for line in file:
-                if 'CONTIENE' in line:
-                    num_archivos = int(line.split()[1])
-                    break    
+    if query is None:    
         return render(request, 'index.html', {'num_juegos': num_archivos})
     elif query == '':
         juegos = []
@@ -79,6 +78,46 @@ def index(request):
                         if n == num_archivos:
                             break
                         n = int(file.readline().strip())
+                        
+    elif query != '':
+        juegos = []
         
-        return render(request, 'index.html', {'num_juegos': num_archivos,'juegos': juegos})
+        # Ejecutar el archivo .bat solo si no hay término de búsqueda
+        subprocess.Popen('database.bat')
+        time.sleep(5)
+        
+        pyautogui.write('7', interval=0.25)
+        pyautogui.write('N', interval=0.25)
+        pyautogui.press('enter')
+        pyautogui.write(query.upper())
+        pyautogui.press('enter')
+        for _ in range(1):
+            pyautogui.write('N')
+            pyautogui.press('enter')
+            time.sleep(1)
+        pyautogui.hotkey('ctrl', 'f9')
+        time.sleep(0.5)
+        
+        with open('Database/DATABASE.TXT', 'r') as file:
+            for line in file:
+                if 'CINTA:' in line:
+                    linea = line.split()
+                    print(linea)
+                    n = linea[-1]
+                    
+                    cinta =(n.split(":")[1])
+                    registro = linea[0]
+                    tipo = linea[-2]
+                    nombre = linea[2:-2]
+                    nombre = ' '.join(nombre)
+                    juegos.append({'n2' : n, 'nombre': nombre, 'tipo': tipo, 'cinta': cinta, 'registro': registro})
+                    if nombre==query:
+                        print("Juego encontrado")
+                        break
+                    if len(juegos) > 1:
+                        juegos = []
+                        break
+
+        
+    return render(request, 'index.html', {'num_juegos': num_archivos,'juegos': juegos})
         
